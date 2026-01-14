32471c4237f54bf77a0d87a375c8f744aba83abe
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-01-14 21:49:23 +0545

    push

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,7 @@
 // Firebase Configuration
 // REPLACE the placeholders with your actual values from the Firebase Console
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDQ56boL_H08lvYEwJFkhzRyFOuP8x2CVs",
-    authDomain: "website-fd610.firebaseapp.com",
-    projectId: "website-fd610",
-    storageBucket: "website-fd610.firebasestorage.app",
-    messagingSenderId: "760043160452",
-    appId: "1:760043160452:web:b0a65d191755b729e393d1",
-    measurementId: "G-SJZN7GYQK4"
-};
+import { firebaseConfig } from './config.js';
 
 // Initialize Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
