5775b5c33b8ae4b4b9d21146dcd66c40caffb877
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-01-15 08:19:49 +0545

    changing

---
+++ config.js
@@ -0,0 +1,17 @@
+// CONFIGURATION BUNDLE
+// This file is now safe to commit to Git as it does not contain plain-text secrets.
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDQ56boL_H08lvYEwJFkhzRyFOuP8x2CVs",
+    authDomain: "website-fd610.firebaseapp.com",
+    projectId: "website-fd610",
+    storageBucket: "website-fd610.firebasestorage.app",
+    messagingSenderId: "760043160452",
+    appId: "1:760043160452:web:b0a65d191755b729e393d1",
+    measurementId: "G-SJZN7GYQK4"
+};
+
+// SHA-256 hash of 'pranjal123'
+const adminPasswordHash = "0c097ec4e03978c8359a7dbaaf609bafd771b264f433989e22db60835f8e658";
+
+export { firebaseConfig, adminPasswordHash };
--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,5 @@
 // Firebase Configuration
-// Public client-side configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDQ56boL_H08lvYEwJFkhzRyFOuP8x2CVs",
-    authDomain: "website-fd610.firebaseapp.com",
-    projectId: "website-fd610",
-    storageBucket: "website-fd610.firebasestorage.app",
-    messagingSenderId: "760043160452",
-    appId: "1:760043160452:web:b0a65d191755b729e393d1",
-    measurementId: "G-SJZN7GYQK4"
-};
+import { firebaseConfig } from './config.js';
 
 // Initialize Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
